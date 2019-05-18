3fcb4561e1dd24b336116b2c086b02c3ea4c3b36
Michael Wisslead <michael.wisslead@gmail.com>
2019-05-18 10:12:08 -0500

    Add generate_color test.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,9 +1,6 @@
 '''command line program usage example'''
 
-from __future__ import print_function
-
 import math
-import argparse
 
 from unittest import TestCase
 
@@ -11,16 +8,26 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-from prettycolors import generate_colors
+from prettycolors import generate_colors, generate_color
 
 
 class TestGenerate(TestCase):
-    def test_generate_color(self):
+    def test_generate_colors(self):
         '''main function to run if ran as main'''
 
         colors = 24
         factor = next(i for i in range(int(math.sqrt(colors)), 0, -1) if colors % i is 0)
         img = np.reshape(generate_colors(colors), (factor, colors//factor, 3))
+
+        plt.imshow(img, interpolation='none')
+        plt.show()
+
+    def test_generate_color(self):
+        '''main function to run if ran as main'''
+
+        colors = 24
+        factor = next(i for i in range(int(math.sqrt(colors)), 0, -1) if colors % i is 0)
+        img = np.reshape([generate_color(i) for i in range(colors)], (factor, colors//factor, 3))
 
         plt.imshow(img, interpolation='none')
         plt.show()
